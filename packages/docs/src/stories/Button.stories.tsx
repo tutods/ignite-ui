d39f5774a3716c566570feb1236841d3b145fdc3
Daniel Sousa <jdaniel.asousa@gmail.com>
2023-07-09 11:32:42 +0100

    feat: button component

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import type { ButtonProps } from '@tutods-ignite-ui/react';
+import { Button } from '@tutods-ignite-ui/react';
+import { ArrowRight } from 'phosphor-react';
+
+export default {
+  component: Button,
+  title: 'Form/Button',
+} as Meta<ButtonProps>;
+
+export const Primary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Send',
+  },
+};
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Create New',
+    variant: 'secondary',
+  },
+};
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Cancel',
+    variant: 'tertiary',
+  },
+};
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Send',
+    size: 'sm',
+  },
+};
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Next Step
+        <ArrowRight weight="bold" />
+      </>
+    ),
+  },
+};
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Send',
+    disabled: true,
+  },
+};
---
+++ packages/react/src/components/Button.tsx
@@ -0,0 +1,97 @@
+import type { ComponentProps, ElementType } from 'react';
+
+import { styled } from '@/styles';
+
+/* eslint-disable sort-keys-fix/sort-keys-fix */
+export const Button = styled('button', {
+  all: 'unset',
+
+  minWidth: 120,
+
+  padding: '0 $4',
+  borderRadius: '$sm',
+  boxSizing: 'border-box',
+  cursor: 'pointer',
+
+  display: 'inline-flex',
+  alignItems: 'center',
+  gap: '$2',
+  justifyContent: 'center',
+
+  fontFamily: '$default',
+  fontSize: '$sm',
+  fontWeight: '$medium',
+  textAlign: 'center',
+
+  svg: {
+    height: '$4',
+    width: '$4',
+  },
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+
+  variants: {
+    size: {
+      sm: {
+        height: 38,
+      },
+
+      md: {
+        height: 46,
+      },
+    },
+
+    variant: {
+      primary: {
+        backgroundColor: '$ignite500',
+        color: '$white',
+
+        '&:not(:disabled):hover': {
+          backgroundColor: '$ignite300',
+        },
+
+        '&:disabled': {
+          backgroundColor: '$gray200',
+        },
+      },
+
+      secondary: {
+        color: '$ignite300',
+        border: '2px solid $ignite500',
+
+        '&:not(:disabled):hover': {
+          backgroundColor: '$ignite500',
+          color: '$white',
+        },
+
+        '&:disabled': {
+          color: '$gray200',
+          borderColor: '$gray200',
+        },
+      },
+
+      tertiary: {
+        color: '$gray100',
+
+        '&:not(:disabled):hover': {
+          color: '$white',
+        },
+
+        '&:disabled': {
+          color: '$gray600',
+        },
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'md',
+    variant: 'primary',
+  },
+});
+
+export type ButtonProps = ComponentProps<typeof Button> & {
+  as?: ElementType;
+};
