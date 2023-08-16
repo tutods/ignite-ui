1cb39317e761972fca6fe485abc92d9b8cd8018e
Daniel Sousa <jdaniel.asousa@gmail.com>
2023-08-16 20:52:20 +0100

    feat: checkbox

--- packages/react/src/components/Checkbox/styles.ts
+++ packages/react/src/components/Checkbox/styles.ts
@@ -1,11 +1,58 @@
 import * as Checkbox from '@radix-ui/react-checkbox';
 
-import { styled } from '@/styles';
+import { styled, keyframes } from '@/styles';
 
 export const CheckboxContainer = styled(Checkbox.Root, {
-  backgroundColor: '',
+  all: 'unset',
+  backgroundColor: '$gray900',
   height: '$6',
   width: '$6',
+  lineHeight: 0,
+  borderRadius: '$xs',
+  cursor: 'pointer',
+  overflow: 'hidden',
+  boxSizing: 'border-box',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  border: '2px solid transparent',
+
+  '&[data-state="checked"]': {
+    backgroundColor: '$ignite300',
+  },
+
+  '&:focus': {
+    border: '2px solid $ingite300',
+  },
 });
 
-export const CheckboxIndicator = styled(Checkbox.Indicator, {});
+const slideIn = keyframes({
+  from: {
+    transform: 'translateY(-100%)',
+  },
+  to: {
+    transform: 'translateY(0)',
+  },
+});
+
+const slideOut = keyframes({
+  from: {
+    transform: 'translateY(0)',
+  },
+  to: {
+    transform: 'translateY(-100%)',
+  },
+});
+
+export const CheckboxIndicator = styled(Checkbox.Indicator, {
+  color: '$white',
+  width: '$4',
+  height: '$4',
+
+  '&[data-state="checked"]': {
+    animation: `${slideIn} 200ms ease-out`,
+  },
+  '&[data-state="unchecked"]': {
+    animation: `${slideOut} 200ms ease-out`,
+  },
+});
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -6,3 +6,4 @@ export * from '@/components/Text';
 export * from '@/components/TextArea';
 export * from '@/components/TextInput';
 export * from '@/components/MultiStep';
+export * from '@/components/Checkbox';
