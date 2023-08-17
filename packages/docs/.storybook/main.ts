451064771ed2575075641c83e00e8837e2240d20
Daniel Sousa <jdaniel.asousa@gmail.com>
2023-08-17 21:40:47 +0100

    fix: add subpath to storybook on prod

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -17,6 +17,13 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignite-ui/';
+    }
+
+    return config;
+  },
 };
 export default config;
 /**
