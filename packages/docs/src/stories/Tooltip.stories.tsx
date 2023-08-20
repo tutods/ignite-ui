fedcb79f5fe05e28625ca376eadbf412998e212a
Daniel Sousa <jdaniel.asousa@gmail.com>
2023-08-20 16:29:30 +0100

    docs: update tooltip story

--- packages/docs/src/stories/Tooltip.stories.tsx
+++ packages/docs/src/stories/Tooltip.stories.tsx
@@ -5,6 +5,13 @@ import type { Meta, StoryObj } from '@storybook/react';
 export default {
   component: Tooltip,
   title: 'Data Display/Tooltip',
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+    },
+  },
   args: {
     content: 'October 26 - Available',
     children: <Button>Day 26</Button>,
